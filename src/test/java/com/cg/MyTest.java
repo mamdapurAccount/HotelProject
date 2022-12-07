3294da3129de0141d11e2afd823e1bf714b53db9
MK <manatharv@gmail.com>
2022-12-07 09:59:26 +0530

    Nikita Creating First Time

---
+++ src/test/java/com/cg/MyTest.java
@@ -0,0 +1,15 @@
+package com.cg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		assertEquals("Hello", "Hello");
+	}
+	
+
+}
